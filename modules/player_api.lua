21af18f2ddaff3a72cc6c85e07b5bcf3a1fbb41b
Dakota <95160810+urdakota@users.noreply.github.com>
2022-02-12 18:47:58 -0500

    Create player_api.lua

---
+++ modules/player_api.lua
@@ -0,0 +1,31 @@
+local players = {}
+local playerapi = {};
+--init
+for i,v in pairs(game.Players:GetChildren()) do table.insert(players, v.Name) end
+game:GetService("Players").PlayerAdded:Connect(function(player) table.insert(players, player.Name) end)
+game:GetService("Players").PlayerRemoving:Connect(function(player) table.remove(players, table.find(players, player.Name)) end)
+
+--modules
+function playerapi:getPlayerId(input)
+    for i = 1, #players do 
+        local v = players[i]
+        if v == input then
+            return i
+        end
+    end
+end
+function playerapi:getPlayerName(input)
+    for i = 1, #players do 
+        local v = players[i]
+        if i == input then 
+            return v
+        end
+    end
+end
+function playerapi:getPlayers()
+    return players
+end
+function playerapi:getPlayerCount()
+    return #players
+end
+return playerapi;
